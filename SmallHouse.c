c9c16c528d788d9cd9a5f6486dc11b99f3989f9c
Peijie Liu <liupe@cip.ifi.lmu.de>
2019-07-03 18:26:01 +0200

    added reduced version of smarthouse

---
+++ SmallHouse.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+extern void __VERIFIER_error();
+extern int __VERIFIER_nondet_int();
+extern void __VERIFIER_assume(int condition);
+
+//Enum Deklaration
+enum Schalter{an, aus};
+enum Fenstersensor{offen, zu};
+
+enum Schalter heizung;
+enum Fenstersensor fensterSensor;
+
+int sensorInitialize(){
+	fensterSensor = zu;
+	return 0;
+}
+
+int initialize(){
+	heizung = an;
+	return 0;
+}
+
+void changeStatePerSensor(){
+	if(fensterSensor == offen){
+		heizung = aus;
+	} 
+}
+
+int main(){
+
+	while(1){
+
+		initialize();
+
+		//CPA Checker
+
+		fensterSensor = __VERIFIER_nondet_int();
+
+		//?
+		__VERIFIER_assume(fensterSensor== offen | fensterSensor == zu);
+
+
+		changeStatePerSensor();
+
+		if (!(fensterSensor==offen || heizung== aus))
+		{
+			__VERIFIER_error();
+		}
+
+	}
+
+	return 0;
+
+
+}
